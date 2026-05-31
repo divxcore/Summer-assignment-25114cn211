19b812932a0c8483c4c2ff6efcbb59b19c7d46a1
divxcore <divxglbitm@gmail.com>
2026-05-31 16:30:35 +0530

    Day-1 uploaded.

---
+++ day1/sum.py
@@ -0,0 +1,8 @@
+def sum_natural_numbers(n):
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+    return total
+
+n = int(input("Enter N: "))
+print(f"Sum = {sum_natural_numbers(n)}")
